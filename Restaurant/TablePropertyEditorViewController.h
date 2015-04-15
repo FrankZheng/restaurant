220ac649f5b41655842f8fa7462071e621acb095
zhengxiaoqiang <zhengxiaoqiang@meituan.com>
2015-04-15 15:00:36 +0800

    Add table property editor xib and controllers

---
+++ Restaurant/TablePropertyEditorViewController.h
@@ -0,0 +1,13 @@
+//
+//  TablePropertyEditorViewController.h
+//  Restaurant
+//
+//  Created by zhengxiaoqiang on 15/4/15.
+//  Copyright (c) 2015年 Frank Zheng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TablePropertyEditorViewController : UIViewController
+
+@end
