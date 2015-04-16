934194990d23ad90415fb55d83285dd0d63a83a2
Frank Zheng <1674414@qq.com>
2015-04-16 21:38:00 +0800

    remove unused files

--- Restaurant/RoundButton.m
+++ Restaurant/RoundButton.m
@@ -32,12 +32,4 @@
     self.layer.cornerRadius = kCornerRadius;
 }
 
-/*
-// Only override drawRect: if you perform custom drawing.
-// An empty implementation adversely affects performance during animation.
-- (void)drawRect:(CGRect)rect {
-    // Drawing code
-}
-*/
-
 @end
