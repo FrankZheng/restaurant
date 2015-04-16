bbaf106820d92ffddc3253d3049a78e076591555
Frank Zheng <1674414@qq.com>
2015-04-16 17:33:33 +0800

    do some cleanup and structure.

--- Restaurant/RoomSwitchViewController.m
+++ Restaurant/RoomSwitchViewController.m
@@ -11,6 +11,9 @@
 #import "Model.h"
 #import "Utils.h"
 
+#define kSwitchViewItemColor 0x5F5C59
+#define kSwithViewItemSelectedColor 0xFFFFFF
+
 @interface RoomSwitchViewController ()
 @property(nonatomic, strong) SlideSwitchView *slideSwitchView;
 @property(nonatomic, strong) NSMutableArray *roomViewControllers;
@@ -23,39 +26,60 @@
     self = [super init];
     
     if(self) {
-        _slideSwitchView = [[SlideSwitchView alloc] initWithFrame:frame];
-        _slideSwitchView.tabItemNormalColor = UIColorFromRGB(0x5f5c59);
-        _slideSwitchView.tabItemSelectedColor = UIColorFromRGB(0xffffff);
-        _slideSwitchView.shadowImage = [[UIImage imageNamed:@"red_line_and_shadow.png"]
-                                        stretchableImageWithLeftCapWidth:59.0f topCapHeight:0.0f];
-        _slideSwitchView.slideSwitchViewDelegate = self;
-        //_slideSwitchView.backgroundColor = [UIColor greenColor];
-        
-        //setup the right side button
-        UIButton *rightSideButton = [UIButton buttonWithType:UIButtonTypeCustom];
-        [rightSideButton setImage:[UIImage imageNamed:@"icon_add_button.png"] forState:UIControlStateNormal];
-        //[rightSideButton setImage:[UIImage imageNamed:@"icon_rightarrow.png"]  forState:UIControlStateHighlighted];
-        rightSideButton.frame = CGRectMake(0, 0, 42.0f, 42.0f);
-        [rightSideButton addTarget:self action:@selector(buttonClicked:) forControlEvents:UIControlEventTouchUpInside];
-        
-        _slideSwitchView.rigthSideButton = rightSideButton;
-        
-        // Do any additional setup after loading the view.
-        [_slideSwitchView buildUI];
-        
-        self.view = _slideSwitchView;
-        
-        _roomViewControllers = [[NSMutableArray alloc] init];
+        //initialization
         _model = [Model shareInstance];
+        _roomViewControllers = [[NSMutableArray alloc] init];
         
+        //init switch view
+        [self initSwitchView:frame];
+
+        //load rooms
+        [self loadRooms];
     }
     return self;
+}
+
+-(void)initSwitchView:(CGRect)frame {
+    
+    //setup switch view
+    _slideSwitchView = [[SlideSwitchView alloc] initWithFrame:frame];
+    _slideSwitchView.tabItemNormalColor = UIColorFromRGB(kSwitchViewItemColor);
+    _slideSwitchView.tabItemSelectedColor = UIColorFromRGB(kSwithViewItemSelectedColor);
+    _slideSwitchView.shadowImage = [[UIImage imageNamed:@"red_line_and_shadow.png"]
+                                    stretchableImageWithLeftCapWidth:59.0f topCapHeight:0.0f];
+    //setup the right side button
+    UIButton *rightSideButton = [UIButton buttonWithType:UIButtonTypeCustom];
+    [rightSideButton setImage:[UIImage imageNamed:@"icon_add_button.png"] forState:UIControlStateNormal];
+    //[rightSideButton setImage:[UIImage imageNamed:@"icon_rightarrow.png"]  forState:UIControlStateHighlighted];
+    rightSideButton.frame = CGRectMake(0, 0, 42.0f, 42.0f);
+    [rightSideButton addTarget:self action:@selector(buttonClicked:) forControlEvents:UIControlEventTouchUpInside];
+    _slideSwitchView.rigthSideButton = rightSideButton;
+    
+    _slideSwitchView.slideSwitchViewDelegate = self;
+    self.view = _slideSwitchView;
+}
+
+
+-(void)loadRooms {
+    //load rooms
+    NSArray* rooms = [_model getRooms];
+    if(rooms.count > 0) {
+        for (Room *room in rooms) {
+            RoomViewController* roomViewController = [[RoomViewController alloc] init];
+            [_roomViewControllers addObject:roomViewController];
+            [roomViewController loadRoom:room];
+        }
+        
+        [_slideSwitchView buildUI];
+        [_slideSwitchView selectTab:0]; //select the first one
+    }
 }
 
 
 - (void)viewDidLoad {
     [super viewDidLoad];
 }
+
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
@@ -109,16 +133,5 @@
 - (void)slideSwitchView:(SlideSwitchView *)view didSelectTab:(NSUInteger)number {
     NSLog(@"didSelectTab, %ld", (long)number);
 }
-
-
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
 
 @end
